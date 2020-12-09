098a666986303a607822bf32d0fddabdfdcfd75c
rodriy20 <rodriy20@users.noreply.github.com>
2020-12-09 14:18:26 -0600

    Completed Activity 5

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -6,6 +6,7 @@ namespace Persistence
     public class DataContext : DbContext
     {
         public DbSet<Value> Values { get; set; }
+        public DbSet<Post> Posts { get; set; }
         public DataContext(DbContextOptions options) : base(options)
         {
         }
