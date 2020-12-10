0ae67c5376fe21c48702a1391cac346d0580a69c
rodriy20 <rodriy20@users.noreply.github.com>
2020-12-10 16:04:13 -0600

    Completed Activity 8

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PostList from './components/PostList/PostList';
+import Post from './components/Post/Post';
+import './App.css';
 
 class App extends React.Component {
   state = {
-    values: []
+    posts: [],
+    post: null
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
+    axios.get('http://localhost:5000/api/posts')
     .then((response) => {
       this.setState({
-        values: response.data
+        posts: response.data
       })
     })
       .catch((error) => {
@@ -19,14 +23,34 @@ class App extends React.Component {
     })
   }
 
-  render() {
+  viewPost = (post) => {
+    console.log(`view ${post.title}`);
+    this.setState({
+      post: post
+    });
+  }
+
+render() {
+    const { posts, post } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          BlogBox
-        </header>
-          {this.state.values.map((value: any) => <div key={value}>{value}</div>)}
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            BlogBox
+          </header>
+          <main className="App-content">
+            <Switch>
+              <Route exact path="/">
+                <PostList posts={posts} clickPost={this.viewPost} />
+              </Route>
+              <Route path="/posts/:postId">
+                <Post post={post} />
+              </Route>
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
